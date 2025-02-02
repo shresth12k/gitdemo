880da8ccf475305ca26400ba4dae1810409f1969
Shresth <shresthkumar111@gmail.com>
2025-02-02 11:35:23 +0530

    newchange

---
+++ cal.java
@@ -0,0 +1,16 @@
+
+public class cal {
+    public int calc(int a,int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        cal c = new cal();
+        int result = c.calc(a, b);
+        System.out.print(result);
+    }
+}
---
+++ main.java
@@ -0,0 +1,14 @@
+class Main  { 
+    public int cal(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        Main c = new Main();
+        int result = c.cal(a, b);
+        System.out.print(result);
+    }
+}
